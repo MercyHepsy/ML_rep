56f6ff7ed2100c085d6eab8e5fedb2c9a78e7e92
Cittatrial <166361064+Cittatrial@users.noreply.github.com>
2024-04-08 12:46:52 +0530

    Update and rename Retail.py to Iris_reg.py

---
+++ Iris_reg.py
@@ -0,0 +1,52 @@
+# Import Dataset from sklearn
+from sklearn.datasets import load_iris
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+
+# Load Iris Data
+iris = load_iris()
+
+# Creating pd DataFrames
+iris_df = pd.DataFrame(data= iris.data, columns= iris.feature_names)
+target_df = pd.DataFrame(data= iris.target, columns= ['species'])
+def converter(specie):
+    if specie == 0:
+        return 'setosa'
+    elif specie == 1:
+        return 'versicolor'
+    else:
+        return 'virginica'
+        
+target_df['species'] = target_df['species'].apply(converter)
+
+# Concatenate the DataFrames
+iris_df = pd.concat([iris_df, target_df], axis= 1)
+
+# pairplot of independent variables
+ax = sns.pairplot(iris_df, hue= 'species')
+st.pyplot(ax.figure)
+
+# Model
+# Converting Objects to Numerical dtype
+iris_df.drop('species', axis= 1, inplace= True)
+target_df = pd.DataFrame(columns= ['species'], data= iris.target)
+iris_df = pd.concat([iris_df, target_df], axis= 1)
+# Variables
+x= iris_df.drop(labels= 'sepal length (cm)', axis= 1)
+y= iris_df['sepal length (cm)']
+
+# Splitting the Dataset 
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size= 0.25, random_state= 20)
+
+# Instantiating LinearRegression() Model
+lr = LinearRegression()
+
+# Training/Fitting the Model
+lr.fit(x_train, y_train)
+
+# Making Predictions
+lr.predict(x_test)
+pred = lr.predict(x_test)
