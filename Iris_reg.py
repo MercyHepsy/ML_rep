677ff9d256294a8c4954b570f12465a5d9634a8a
Cittatrial <166361064+Cittatrial@users.noreply.github.com>
2024-04-08 16:04:44 +0530

    Update Iris_reg.py

--- Iris_reg.py
+++ Iris_reg.py
@@ -81,6 +81,6 @@ pred = lr.predict(x_test)
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 MAE =  mean_absolute_error(y_test, pred)
-MSE = mean_squared_error(y_test, pred))
+MSE = mean_squared_error(y_test, pred)
 
 st.write(f"The Mean Absolute Error is, **{MAE}**")
