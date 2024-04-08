ac4820faebfc087d8d9393efd686464a6da564e2
Cittatrial <166361064+Cittatrial@users.noreply.github.com>
2024-04-08 15:17:06 +0530

    Update Iris_reg.py

--- Iris_reg.py
+++ Iris_reg.py
@@ -32,6 +32,22 @@ iris_df = pd.concat([iris_df, target_df], axis= 1)
 ax = sns.pairplot(iris_df, hue= 'species')
 st.pyplot(ax.figure)
 
+fig, axes = plt.subplots(2, 2, figsize=(10,10)) 
+  
+axes[0,0].set_title("Sepal Length") 
+axes[0,0].hist(df['SepalLengthCm'], bins=7) 
+  
+axes[0,1].set_title("Sepal Width") 
+axes[0,1].hist(df['SepalWidthCm'], bins=5) 
+  
+axes[1,0].set_title("Petal Length") 
+axes[1,0].hist(df['PetalLengthCm'], bins=6)
+  
+axes[1,1].set_title("Petal Width") 
+axes[1,1].hist(df['PetalWidthCm'], bins=6)
+
+st.pyplot(fig)
+
 # Model
 # Converting Objects to Numerical dtype
 iris_df.drop('species', axis= 1, inplace= True)
