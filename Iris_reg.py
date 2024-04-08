6e0f5010b5d80c749d67d9d87ef31f1e81f23a8d
Cittatrial <166361064+Cittatrial@users.noreply.github.com>
2024-04-08 16:11:25 +0530

    Update Iris_reg.py

--- Iris_reg.py
+++ Iris_reg.py
@@ -9,6 +9,14 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
+st.set_page_config(page_title="Iris Data Analysis", layout="wide")
+
+# Page Layout
+c1, c2 = st.columns([0.3,0.7])
+
+with c2:
+    st.markdown("## Hospital Re-Admission Analysis")
+
 # Load Iris Data
 iris = load_iris()
 
@@ -52,13 +60,13 @@ st.markdown("### Relationship between Variables")
 ax = sns.pairplot(iris_df, hue= 'species')
 st.pyplot(ax.figure)
 
-st.write(iris_df.columns)
-
 # Model
 # Converting Objects to Numerical dtype
 iris_df.drop('species', axis= 1, inplace= True)
 target_df = pd.DataFrame(columns= ['species'], data= iris.target)
 iris_df = pd.concat([iris_df, target_df], axis= 1)
+
+st.dataframe(iris_df.head())
 
 # Variables
 x= iris_df.drop(labels= 'sepal length (cm)', axis= 1)
@@ -73,14 +81,15 @@ lr = LinearRegression()
 # Training/Fitting the Model
 lr.fit(x_train, y_train)
 
-# Making Predictions
-lr.predict(x_test)
-pred = lr.predict(x_test)
-
-# MAE
-from sklearn.metrics import mean_absolute_error, mean_squared_error
-
-MAE =  mean_absolute_error(y_test, pred)
-MSE = mean_squared_error(y_test, pred)
-
-st.write(f"The Mean Absolute Error is, **{MAE}**")
+if st.button('Predict'):    
+	# Making Predictions
+    lr.predict(x_test)
+    pred = lr.predict(x_test)
+    
+    # MAE
+    from sklearn.metrics import mean_absolute_error, mean_squared_error
+    
+    MAE =  mean_absolute_error(y_test, pred)
+    MSE = mean_squared_error(y_test, pred)
+    
+    st.write(f"The Mean Absolute Error is, **{MAE}**")
